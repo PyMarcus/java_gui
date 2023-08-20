2282c2ae358862878e8e642e06c9ffa9f3967df6
Marcus <88283829+PyMarcus@users.noreply.github.com>
2023-08-19 21:38:03 -0300

    Add files via upload

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,5 +1,48 @@
 package calculator;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
 public class Calculator extends CalculatorForm {
 
+
+    @Override
+    protected void btnClose(ActionEvent ev) {
+        if(this.confirmToClose()){
+            this.setVisible(false);
+            this.dispose();
+        }
+    }
+
+    @Override
+    protected void btnClear(ActionEvent ev) {
+        textCapital.setText("");
+        textAmount.setText("");
+        textRate.setText("");
+        textPeriod.setText("");
+    }
+
+    @Override
+    protected void btnSolve(ActionEvent ev) {
+        double capital = Double.parseDouble(textCapital.getText());
+        double rate = Double.parseDouble(textRate.getText());
+        double period = Double.parseDouble(textPeriod.getText());
+
+        double amount = capital * Math.pow((1 + rate), period);
+        textAmount.setText(Double.toString(amount));
+    }
+
+    private boolean confirmToClose(){
+        int choise = JOptionPane.showConfirmDialog(
+                this,
+                "Are u sure?",
+                "Close",
+                JOptionPane.YES_NO_OPTION
+        );
+
+        if(choise == JOptionPane.YES_NO_OPTION){
+            return true;
+        }
+        return false;
+    }
 }
--- src/calculator/CalculatorForm.java
+++ src/calculator/CalculatorForm.java
@@ -1,8 +1,12 @@
 package calculator;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public abstract class CalculatorForm extends JFrame {
+
+    private static final int LABEL_SIZE = 12;
 
     protected JPanel rodape;
     protected JPanel form;
@@ -21,26 +25,84 @@ public abstract class CalculatorForm extends JFrame {
 
     public CalculatorForm(){
         this.run();
+        this.events();
     }
 
     private void run(){
         this.setTitle("Simple Calculator");
-        this.setSize(640, 480);
+        //this.setSize(640, 480);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        // main layout
+        this.setResizable(false);
+        this.getContentPane().setLayout(new BorderLayout());
+        this.getContentPane().add(getForm(), BorderLayout.CENTER);
+        this.getContentPane().add(getRodape(), BorderLayout.PAGE_END);
+
+        // resize automatically
+        this.pack();
     }
 
     public JPanel getRodape(){
         if(rodape == null){
-            rodape = new JPanel();
+            rodape = new JPanel(new FlowLayout(FlowLayout.CENTER));
         }
 
+        solve = new JButton("Solve");
+        clear = new JButton("Clear");
+        close = new JButton("Close");
+        this.customizeButton();
+
+        rodape.add(solve);
+        rodape.add(clear);
+        rodape.add(close);
         return rodape;
     }
 
     public JPanel getForm(){
         if(form == null){
-            form = new JPanel();
+            form = new JPanel(new GridLayout(4, 2));
+            lblCapital = new JLabel("Capital");
+            textCapital = new JTextField(LABEL_SIZE);
+
+            lblRate = new JLabel("Rate");
+            textRate = new JTextField(LABEL_SIZE);
+
+            lblPeriod = new JLabel("Period");
+            textPeriod = new JTextField(LABEL_SIZE);
+
+            lblAmount = new JLabel("Amount");
+            textAmount = new JTextField(LABEL_SIZE);
+            textAmount.setEditable(false);
+
+            form.add(lblCapital);
+            form.add(textCapital);
+            form.add(lblRate);
+            form.add(textRate);
+            form.add(lblPeriod);
+            form.add(textPeriod);
+            form.add(lblAmount);
+            form.add(textAmount);
         }
 
         return form;
     }
+
+    private void customizeButton(){
+        //color
+        close.setBackground(Color.RED);
+        close.setForeground(Color.WHITE);
+    }
+
+    protected abstract void btnClose(ActionEvent ev);
+
+    protected abstract void btnClear(ActionEvent ev);
+
+    protected abstract void btnSolve(ActionEvent ev);
+
+    private void events(){
+        close.addActionListener(this::btnClose);
+        clear.addActionListener(this::btnClear);
+        solve.addActionListener(this::btnSolve);
+    }
+
 }
